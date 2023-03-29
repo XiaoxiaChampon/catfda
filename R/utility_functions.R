80855017ea85553ba879a765b936b84d35400199
XiaoxiaChampon <63108113+XiaoxiaChampon@users.noreply.github.com>
2023-03-28 22:47:58 -0400

    Update utility_functions.R

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -15,6 +15,8 @@ library(dbscan)
 library(fossil)
 library(NbClust)
 library(ggplot2)
+library(stats)
+library(graphics)
 
 #Function to return the logit
 logit <- function(x){
@@ -521,7 +523,87 @@ randfn=function(dbcluster,kcluster,nsub){
   randkmeans=rand.index(tlabel,kcluster)
   return(list("randd"=randdbscan,"randk"=randkmeans))
 }
-
-
+####elbow method
+elbow=function (data, plot = TRUE) {
+    if (missing(data)) {
+        stop("Please provide a two-columns data frame.")
+    }
+    if (!is.list(data)) {
+        stop("`data` must be a two-columns data frame.")
+    }
+    if (ncol(data) > 2) {
+        warning("Only the first two columns will be considered.")
+    }
+    if (!is.numeric(data[, 1]) || !is.numeric(data[, 2])) {
+        stop("Non-numeric data detected.")
+    }
+    if (sum(is.na(data[, 1])) + sum(is.na(data[, 2]))) {
+        stop("Missing values detected.")
+    }
+    if (!is.logical(plot)) {
+        stop("`plot` must be a boolean.")
+    }
+    data <- data[, 1:2]
+    data <- data[order(data[, 1]), ]
+    constant <- data[c(1, nrow(data)), ]
+    colnames(constant) <- c("x", "y")
+    mod <- stats::lm(y ~ x, data = constant)
+    data[, "constant"] <- round(mod$coef[[1]] + mod$coef[[2]] * 
+        data[, 1], 3)
+    pos <- round(nrow(data)/2)
+    if (data[pos, "constant"] < data[pos, 2]) {
+        ymin <- min(data[, 2])
+        data[, "benefits"] <- ymin + round(data[, 2] - data[, 
+            "constant"], 3)
+        maxi <- data[which.max(data[, "benefits"]), ]
+    }
+    else {
+        ymax <- max(data[, 2])
+        data[, "benefits"] <- ymax - round(data[, "constant"] - 
+            data[, 2], 3)
+        maxi <- data[which.min(data[, "benefits"]), ]
+    }
+    xxx <- list()
+    xxx[[1]] <- maxi[1, 1]
+    xxx[[2]] <- data
+    names(xxx) <- c(paste(colnames(data)[1], "selected", sep = "_"), 
+        "data")
+    if (plot) {
+        xlims <- range(data[, 1])
+        ylims <- c(min(c(data[, 2], data[, 3], data[, 4])), max(data[, 
+            2]))
+        graphics::par(mar = c(2.5, 2.5, 1.5, 1.5), family = "serif", 
+            cex.axis = 0.85, mgp = c(2, 0.15, 0), tcl = -0.25, 
+            fg = "#666666", col = "#666666", col.axis = "#666666")
+        graphics::plot(x = data[, 1], y = data[, 2], xlim = xlims, 
+            ylim = ylims, ann = FALSE, axes = FALSE, type = "n")
+        graphics::grid()
+        graphics::box()
+        graphics::par(mgp = c(2, 0, 0))
+        graphics::axis(1, lwd = 0)
+        graphics::axis(1, maxi[, 1], lwd = 0, font = 2, col.axis = "black")
+        graphics::par(mgp = c(2, 0.25, 0))
+        graphics::axis(2, lwd = 0, las = 1)
+        at <- round(maxi[, 2], 3)
+        graphics::axis(2, at, lwd = 0, font = 2, col.axis = "black", 
+            las = 1)
+        graphics::mtext(side = 1, cex = 1, line = 1.25, text = expression("x"))
+        graphics::mtext(side = 2, cex = 1, line = 1.45, text = expression("f(x)"))
+        graphics::polygon(x = c(data[, 1], data[1, 1]), y = c(data[, 
+            "benefits"], data[1, "benefits"]), col = "#aaaaaa66", 
+            border = "#aaaaaa")
+        graphics::points(x = data[, 1], y = data[, 2], type = "b", 
+            pch = 19, col = "black")
+        graphics::lines(x = rep(maxi[1, 1], 2), y = c(par()$usr[3], 
+            maxi[1, 2]), col = "black", lwd = 0.5)
+        graphics::lines(x = c(par()$usr[1], maxi[1, 1]), y = rep(maxi[1, 
+            2], 2), col = "black", lwd = 0.5)
+        graphics::points(x = maxi[, 1], y = maxi[, 2], type = "b", 
+            pch = 19, cex = 1.5, col = "black")
+        graphics::points(x = maxi[, 1], y = maxi[, 4], type = "b", 
+            pch = 19, cex = 1, col = "#666666")
+    }
+    return(xxx)
+}
 
 
